be8fcf1bf4cf1c3a5210d29f5e4d99241bf97206
Amanda Mitchell <amandamitchell707@gmail.com>
2018-10-31 13:36:19 -0700

    Move audio files to audio folder

--- app/index.js
+++ app/index.js
@@ -1,88 +1,106 @@
-import '../assets/styles/index.scss';
+// import "../assets/styles/index.scss";
 
-let audioContext = new (window.AudioContext || window.webkitAudioContext)();
-let oscillatorList = [];
-let masterGainNode = null;
+// let audioContext = new (window.AudioContext || window.webkitAudioContext)();
+// let oscillatorList = [];
+// let masterGainNode = null;
 
-let keyboard = document.querySelector('.keyboard');
-let waveSelector = document.querySelector("select[name='waveform']");
-let volumeControl = document.querySelector("input[name='volume']");
+// let keyboard = document.querySelector('.keyboard');
+// let waveSelector = document.querySelector("select[name='waveform']");
+// let volumeControl = document.querySelector("input[name='volume']");
 
-let noteFreq = null;
-let customWaveform = null;
-let sineTerms = null;
-let cosineTerms = null;
+// let noteFreq = null;
+// let customWaveform = null;
+// let sineTerms = null;
+// let cosineTerms = null;
 
-function createNoteTable() {
-  let noteFreq = [];
-  for (let i = 0; i < 3; i++) {
-    noteFreq[i] = [];
-  }
+// function createNoteTable() {
+//   let noteFreq = [];
+//   for (let i = 0; i < 4; i++) {
+//     noteFreq[i] = [];
+//   }
 
-  noteFreq[0]["F"] = 87.307057858;
-  noteFreq[0]["F#"] = 92.498605678;
-  noteFreq[0]["G"] = 97.998858995;
-  noteFreq[0]["G#"] = 103.826174395;
-  noteFreq[1]["A"] = 110.0;
-  noteFreq[1]["A#"] = 116.54094038;
-  noteFreq[1]["B"] = 123.470825314;
-  noteFreq[1]["C"] = 130.81278265;
-  noteFreq[1]["C#"] = 138.591315488;
-  noteFreq[1]["D"] = 146.832383959;
-  noteFreq[1]["D#"] = 155.563491861;
-  noteFreq[1]["E"] = 164.813778456;
-  noteFreq[1]["F"] = 174.614115717;
-  noteFreq[1]["F#"] = 184.997211356;
-  noteFreq[1]["G"] = 195.997717991;
-  noteFreq[1]["G#"] = 207.65234879;
-  noteFreq[2]["A"] = 220.0;
-  noteFreq[2]["A#"] = 233.081880759;
-  noteFreq[2]["B"] = 246.941650628;
-  noteFreq[2]["C"] = 261.6255653;
-  noteFreq[2]["C#"] = 277.182630976;
-  noteFreq[2]["D"] = 293.664767918;
-  noteFreq[2]["D#"] = 311.126983722;
-  noteFreq[2]["E"] = 329.627556912;
-  noteFreq[2]["F"] = 349.228231434;
-  noteFreq[2]["F#"] = 369.994422712;
-  noteFreq[2]["G"] = 391.995435982;
-  noteFreq[2]["G#"] = 415.30469758;
-  noteFreq[3]["A"] = 440.0;
-  noteFreq[3]["A#"] = 466.163761518;
-  noteFreq[3]["B"] = 493.883301256;
-}
+//   noteFreq[0]["F"] = 87.307057858;
+//   noteFreq[0]["F#"] = 92.498605678;
+//   noteFreq[0]["G"] = 97.998858995;
+//   noteFreq[0]["G#"] = 103.826174395;
+//   noteFreq[1]["A"] = 110.0;
+//   noteFreq[1]["A#"] = 116.54094038;
+//   noteFreq[1]["B"] = 123.470825314;
+//   noteFreq[1]["C"] = 130.81278265;
+//   noteFreq[1]["C#"] = 138.591315488;
+//   noteFreq[1]["D"] = 146.832383959;
+//   noteFreq[1]["D#"] = 155.563491861;
+//   noteFreq[1]["E"] = 164.813778456;
+//   noteFreq[1]["F"] = 174.614115717;
+//   noteFreq[1]["F#"] = 184.997211356;
+//   noteFreq[1]["G"] = 195.997717991;
+//   noteFreq[1]["G#"] = 207.65234879;
+//   noteFreq[2]["A"] = 220.0;
+//   noteFreq[2]["A#"] = 233.081880759;
+//   noteFreq[2]["B"] = 246.941650628;
+//   noteFreq[2]["C"] = 261.6255653;
+//   noteFreq[2]["C#"] = 277.182630976;
+//   noteFreq[2]["D"] = 293.664767918;
+//   noteFreq[2]["D#"] = 311.126983722;
+//   noteFreq[2]["E"] = 329.627556912;
+//   noteFreq[2]["F"] = 349.228231434;
+//   noteFreq[2]["F#"] = 369.994422712;
+//   noteFreq[2]["G"] = 391.995435982;
+//   noteFreq[2]["G#"] = 415.30469758;
+//   noteFreq[3]["A"] = 440.0;
+//   noteFreq[3]["A#"] = 466.163761518;
+//   noteFreq[3]["B"] = 493.883301256;
 
-function setupKeyboard() {
-    noteFreq = createNoteTable();
+//   return noteFreq;
+// }
 
-    volumeControl.addEventListener("change", changeVolume, false);
+// function setupKeyboard() {
+//     noteFreq = createNoteTable();
 
-    masterGainNode = audioContext.createGain();
-    masterGainNode.connect(audioContext.destination);
-    masterGainNode.gain.value = volumeControl.value;
-}
+//     volumeControl.addEventListener("change", changeVolume, false);
 
-function changeVolume(e) {
-    masterGainNode.gain.value = volumeControl.value;
-}
+//     masterGainNode = audioContext.createGain();
+//     masterGainNode.connect(audioContext.destination);
+//     masterGainNode.gain.value = volumeControl.value;
 
-function createWhiteKey(note, octave, freq, xValue) {
-  let keyElement = document.createElement("rect");
+//     // create white keys
+//     noteFreq.forEach((keys, idx) => {
+//       let keyList = Object.entries(keys);
+//       let keyboardElement = document.getElementById("piano");
+//       // debugger;
+//       let keyIndex = 0;
+//       keyList.forEach((key) => {
+//         if (key[0].length == 1) {
+//           keyboardElement.appendChild(createWhiteKey(key[0], idx, key[1], (keyIndex * 100)));
+//           idx += 1;
+//         }
+//       });
+//     });
+// }
 
-  keyElement.className = "white-key";
-  keyElement.dataset["note"] = note;
-  keyElement.dataset["octave"] = octave;
-  keyElement.dataset["frequency"] = freq;
+// function changeVolume(e) {
+//     masterGainNode.gain.value = volumeControl.value;
+// }
 
-  keyElement.x = xValue;
-  keyElement.y = 5;
-  keyElement.rx = 6;
-  keyElement.ry = 6;
-  keyElement.width = 50;
-  keyElement.height = 250;
+// function createWhiteKey(note, octave, freq, xValue) {
+//   let keyElement = document.createElement("rect");
 
-  keyElement.addEventListener("keydown", notePressed, false);
-  keyElement.addEventListener("keyup", noteReleased, false);
+//   keyElement.className = "white-key";
+//   keyElement.dataset["note"] = note;
+//   keyElement.dataset["octave"] = octave;
+//   keyElement.dataset["frequency"] = freq;
 
-  return keyElement;
-}
+//   keyElement.x = xValue;
+//   keyElement.y = 5;
+//   keyElement.rx = 6;
+//   keyElement.ry = 6;
+//   keyElement.width = 50;
+//   keyElement.height = 250;
+
+//   keyElement.addEventListener("keydown", notePressed, false);
+//   keyElement.addEventListener("keyup", noteReleased, false);
+
+//   return keyElement;
+// }
+
+// setupKeyboard();
