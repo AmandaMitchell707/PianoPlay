00db11617c3c2275dec9f989e7cc49e85c8be4f7
Amanda Mitchell <amandamitchell707@gmail.com>
2018-11-01 11:12:48 -0700

    Make note play only once when key is held down

--- app/index.js
+++ app/index.js
@@ -1,10 +1,19 @@
+let keyEnabledArray = Array(31).fill(true);
+
 window.addEventListener('keydown', (e) => {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if (!audio) return;
-  audio.currentTime = 0;
-  audio.play();
-  key.classList.add('playing');
+  if (keyEnabledArray[e.keyCode]) {
+    keyEnabledArray[e.keyCode] = false;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+  }
+});
+
+window.addEventListener('keyup', (e) =>{
+  keyEnabledArray[e.keyCode] = true;
 });
 
 function removeTransition(e) {
