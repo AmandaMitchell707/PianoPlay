24ff5d2f04e32b9de1bbb7c471835943b001d668
Amanda Mitchell <amandamitchell707@gmail.com>
2018-11-04 20:55:01 -0800

    Add cello audio files to setupAudioTags function

--- app/index.js
+++ app/index.js
@@ -19,7 +19,7 @@ window.addEventListener("DOMContentLoaded", () => {
     '4_csharp', '4_dsharp', '4_fsharp', '4_gsharp', '4_asharp'
   ];
 
-  let voices = ['classic', 'electric'];
+  let voices = ['classic', 'cello', 'electric'];
 
   function createAudioTag(key, note, voice) {
     let audioElement = document.createElement('audio');
@@ -37,6 +37,7 @@ window.addEventListener("DOMContentLoaded", () => {
     for (let i = 0; i < dataKeys.length; i++) {
       audioContainer.appendChild(createAudioTag(dataKeys[i], notes[i], voices[0]));
       audioContainer.appendChild(createAudioTag(dataKeys[i], notes[i], voices[1]));
+      audioContainer.appendChild(createAudioTag(dataKeys[i], notes[i], voices[2]));
     }
 
     let body = document.querySelector("body");
