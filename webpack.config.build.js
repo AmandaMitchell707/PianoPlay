eab85140cfc7ed9d9e08fc229ff3a5c703eb4f07
Amanda Mitchell <amandamitchell707@gmail.com>
2018-11-05 10:31:21 -0800

    was on the wrong branch

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -8,12 +8,12 @@ module.exports = merge(webpackConfig, {
     devtool: 'source-map',
 
     output: {
-        path: path.join(__dirname, 'dist'),
+        path: path.join(__dirname, 'build'),
         filename: '[name].[chunkhash].js'
     },
 
     plugins: [
-        new CleanWebpackPlugin(['dist'])
+        new CleanWebpackPlugin(['build'])
     ]
 
 });
